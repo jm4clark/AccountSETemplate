4adcc4824fc628af9e45811c5e3bad0b87fe0981
jm4clark <jamesmclark96@gmail.com>
2019-06-04 15:46:59 +0100

    updated account repo methods

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -2,4 +2,53 @@ package com.qa.persistence.domain;
 
 public class Account {
 
+	// This class needs to have:
+	// An id
+	private static int counter = 0;
+	private int id;
+	// An Account Number
+	private int accNum;
+	// A First Name
+	private String firstName;
+	// A last Name
+	private String lastName;
+	
+	public Account(int accNum, String firstName, String lastName) {
+		this.accNum = accNum;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		
+		counter++;
+		this.id = counter;
+	}
+	
+	public long getID() {
+		return id;
+	}
+
+	public int getAccNum() {
+		return accNum;
+	}
+
+	public void setAccNum(int accNum) {
+		this.accNum = accNum;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+
 }
--- src/main/java/com/qa/persistence/repository/AccountMapRepository.java
+++ src/main/java/com/qa/persistence/repository/AccountMapRepository.java
@@ -1,11 +1,16 @@
 package com.qa.persistence.repository;
 
+
+
 import java.util.HashMap;
 import java.util.Map;
 
 import com.qa.persistence.domain.Account;
+import com.qa.util.JSONUtil;
 
 public class AccountMapRepository implements AccountRepository{
+	
+	JSONUtil jsonutil = new JSONUtil();
 	
 	Map<Long, Account> accountMap = new HashMap<Long, Account>();
 	
@@ -13,23 +18,24 @@ public class AccountMapRepository implements AccountRepository{
 	//do not change the method signature
 
 	public String getAllAccounts() {
-		// TODO Auto-generated method stub
-		return null;
+		return accountMap.toString();
 	}
 
 	public String createAccount(String account) {
-		// TODO Auto-generated method stub
-		return null;
+		Account acc = jsonutil.getObjectForJSON(account, Account.class);
+		accountMap.put(acc.getID(), acc);
+		return "Created account";
 	}
 
 	public String deleteAccount(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		accountMap.remove(id);
+		return "Deleted account";
 	}
 
 	public String updateAccount(Long id, String account) {
-		// TODO Auto-generated method stub
-		return null;
+		Account newAcc = jsonutil.getObjectForJSON(account, Account.class);
+		accountMap.put(newAcc.getID(), newAcc);
+		return "Updated account";
 	}
 
 }
